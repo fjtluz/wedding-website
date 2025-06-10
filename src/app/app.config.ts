b719bc6dd0b85ec6f5e4c2e2428da240f42e8661
fjtluz <fjtluz@proton.me>
2025-06-10 15:05:29 -0300

    Adicionando # nas rotas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 import { routes } from './app.routes';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
       provideZoneChangeDetection({ eventCoalescing: true }), 
       provideHttpClient(),
-      provideRouter(routes),
+      provideRouter(routes, withHashLocation()),
       provideAnimationsAsync()
   ]
 };
